dd6110d61538af6674229531a010fa5650cf0358
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2021-06-14 07:19:15 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,9 @@
+var a="abc";
+//scope of a is global can be accessed from anywhere;
+function scopeDemo()
+{
+    var i = 1;
+    console.log(i);// scope of i is within this function
+}
+scopeDemo();
+console.log(a);
