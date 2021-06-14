be874159b9be111af752d75217d1a2a5702335eb
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2021-06-14 07:14:20 +0530

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -10,4 +10,5 @@ hoistDemo();
 var j=120;
 //in this case the variable is defined after but still works concept called hoisting but would define
 //the variable as undefined similarly in case of hoistDemo
-//all the variables declaration are moved to top of the function 
+//all the variables declaration are moved to top of the function but the value  hasn't been assigned
+
